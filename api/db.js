ce91e8474194d46cc3531b6ca94541739884cb92
ErwannGui <erwann.guillevic@ynov.com>
2019-01-20 20:10:31 +0100

    db auth

--- api/db.js
+++ api/db.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const config = require('config.json');
 var router = express.Router();
 
-exports.connect = function(username, password) {
+function connect(username, password) {
 	mongoose.Promise = global.Promise;
 	//mongoose.connect('mongodb://localhost:27017/ionic');
 	mongoose.connect('mongodb+srv://'+username+':'+password+'@cours-8uau7.mongodb.net/ionic', { useNewUrlParser: true })
@@ -12,6 +13,37 @@ exports.connect = function(username, password) {
 	.catch(err => {
 		console.error(err);
 	});
+};
+
+exports.authToMongo = function() {
+
+	if (mongoose.connection.readyState == 1) {
+		mongoose.connection.close();
+	}
+
+	let username = undefined,
+		password = undefined;
+
+	switch (rights) {
+		case "readWrite":
+			username = 'ionic';
+			password = config.passwords[rights];
+			break;
+		case "dbAdmin":
+			username = 'moderator';
+			password = config.passwords[rights];
+			break;
+		case "su":
+			username = 'admin';
+			password = config.passwords[rights];
+			break;
+		default:
+			username = 'guest';
+			password = config.passwords[rights];
+			break;
+	}
+
+	connect(username, password);
 };
 
 /* Ajout de la fonction d'authentification */
